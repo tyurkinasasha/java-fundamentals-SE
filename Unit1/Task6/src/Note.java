f47aec98dcb0c370431f990b8f22faecf64687d7
Tyurkina Alexandra <tyurkina.sasha@list.ru>
2017-04-26 03:20:37 +0300

    task6 (with .gitignore)

---
+++ Unit1/Task6/src/Note.java
@@ -0,0 +1,11 @@
+/**
+ * Created by hp on 25.04.2017.
+ */
+public class Note {
+    public String content;
+    public String header;
+    public Note(String header,String content){
+        this.header=header;
+        this.content=content;
+    }
+}
